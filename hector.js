d9ed90b87f9e4eb50697aa03e268676d3c333a69
Ilja Everilä <saarni@lakka.kapsi.fi>
2013-09-10 17:23:38 +0300

    A method for moving between scenes

--- hector.js
+++ hector.js
@@ -125,8 +125,6 @@
     });
 
     Crafty.scene("house", function () {
-        var hector;
-
         // Draw room
         Crafty.background(getColor(0));
         Crafty.e("2D, Canvas, Image").image("hector-room2.png").attr({x: 48, y: 48});
@@ -143,11 +141,7 @@
         Crafty.e("Boundary").createBoundary({x: 592, y: 0, w: 48,  h: 480});
 
         // Create player
-        hector = Crafty.e("Hector").attr({x: 200, y: 280});
-
-        hector.bind("consoleinput", function (cmdline) {
-            this.runCommand(cmdline);
-        });
+        Crafty.e("Hector").attr({x: 200, y: 280});
 
         console.setBgColor(null);
         console.setFgColor(1);
@@ -157,25 +151,25 @@
     });
 
     Crafty.scene("bedroom", function () {
-        var hector;
-
         // Draw room
         Crafty.background(getColor(0));
         Crafty.e("2D, Canvas, Image").image("hector-room3.png").attr({x: 48, y: 48});
 
         // Create boundaries
         Crafty.e("Boundary").createBoundary({x: 0,   y: 0,   w: 640, h: 211});
-        Crafty.e("Boundary").createBoundary({x: 0,   y: 384, w: 640, h: 96});
         Crafty.e("Boundary").createBoundary({x: 0,   y: 0,   w: 100, h: 480});
         Crafty.e("Boundary").createBoundary({x: 540, y: 0,   w: 100, h: 480});
         Crafty.e("Boundary").createBoundary({x: 100, y: 210, w: 110, h: 174}, new Crafty.polygon([0, 0], [109, 0], [109, 1], [0, 96]));
         Crafty.e("Boundary").createBoundary({x: 430, y: 210, w: 110, h: 174}, new Crafty.polygon([0, 0], [109, 0], [109, 96], [0, 1]));
+        // Move back on hitting the "4. wall"
+        Crafty.e("2D, Collision").attr({x: 0,   y: 384, w: 640, h: 96})
+            .collision()
+            .onHit("Hector", function () {
+                Crafty.scene("house");
+            });
 
         // Create player
-        hector = Crafty.e("Hector").attr({x: 200, y: 280});
-        hector.bind("consoleinput", function (cmdline) {
-            hector.runCommand(cmdline);
-        });
+        Crafty.e("Hector").attr({x: 200, y: 280});
 
         console.setBgColor(null);
         console.setFgColor(1);
@@ -446,6 +440,10 @@
                         this.y -= objs[i].normal.y * objs[i].overlap;
                     }
                 });
+
+            this.bind("consoleinput", function (cmdline) {
+                this.runCommand(cmdline);
+            });
         },
         actions: {
             HELP: "Try to 'look' at things.",
