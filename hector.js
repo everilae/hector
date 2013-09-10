f0001a30d843bdb3194ac1ea3b4e13f54b5c7aae
Ilja Everilä <saarni@lakka.kapsi.fi>
2013-09-10 13:38:39 +0300

    Sine intro text

--- hector.js
+++ hector.js
@@ -191,12 +191,33 @@
 
         console.setBgColor(null);
         console.setFgColor(4);
-        console.clear();
-        console.put("Press space", 4, 11);
+
+        var sinLookup = [0, 1, 3, 4, 4, 4, 3, 2, 1, -1, -2, -3, -4, -4, -4, -3, -1],
+            slLength = sinLookup.length,
+            text = "Press space",
+            xPos = 4, yPos = 11,
+            enterFrameCallback;
+
+        Crafty.bind("EnterFrame", enterFrameCallback = function (e) {
+            var i, l;
+
+            if (e.frame % 5 === 0) {
+                // The easy way, since we do not have to worry about any other text
+                // on screen
+                console.clear();
+
+                for (i = 0, l = text.length; i < l; i++) {
+                    console.put(text.charAt(i), (xPos + i) % 34, yPos + sinLookup[(xPos + i) % slLength]);
+                }
+
+                xPos = (xPos + 1) % 34;
+            }
+        });
 
         Crafty.bind("KeyDown", function keyDownCallback(e) {
             if (e.key === Crafty.keys.SPACE) {
                 Crafty.unbind("KeyDown", keyDownCallback);
+                Crafty.unbind("EnterFrame", enterFrameCallback);
                 Crafty.scene("menu");
             }
         });
