486026745334730393ed8adc4dfe901a779edadf
Ilja Everilä <saarni@lakka.kapsi.fi>
2013-09-12 16:23:55 +0300

    Added a moving sprite, a tortoise called Amppa

--- hector.js
+++ hector.js
@@ -79,7 +79,8 @@
                 "rubikscube.png",
                 "hector-room2.png",
                 "hector-room3.png",
-                "crackmask.png"
+                "crackmask.png",
+                "amppa.png"
             ],
             function () {
                 Crafty.sprite("hector.png", {
@@ -87,6 +88,9 @@
                 });
                 Crafty.sprite("rubikscube.png", {
                     rubikscube: [0, 0, 16, 16]
+                });
+                Crafty.sprite("amppa.png", {
+                        amppa: [0, 0, 32, 16]
                 });
                 loaded = true;
             }
@@ -168,8 +172,9 @@
                 Crafty.scene("house");
             });
 
+        Crafty.e("Amppa").attr({x: 300, y: 280}).setBoundary(new Crafty.polygon([210, 280], [430, 280], [430, 384], [210, 384]));
         // Create player
-        Crafty.e("Hector").attr({x: 200, y: 280});
+        Crafty.e("Hector").attr({x: 200, y: 281});
 
         console.setBgColor(null);
         console.setFgColor(1);
@@ -611,6 +616,80 @@
                     }
                 }
             };
+        }
+    });
+
+    Crafty.c("Amppa", {
+        init: function () {
+            this.requires("2D, DOM, amppa, SpriteAnimation, Item");
+            this.w = 32;
+            this.h = 16;
+
+            this._moveChance = 0.02;
+            this._changeDirChance = 0.1;
+            // initial direction
+            this.changeDir();
+
+            this.animate("walk", [[0, 0], [32, 0], [64, 0], [96, 0]]);
+
+            this.actions = {
+                LOOK: function (actor) {
+                    actor.say("A tortoise. How exiting.");
+                }
+            };
+
+            this.bind("EnterFrame", function (e) {
+                this.frameHandler(e);
+            });
+        },
+
+        _directions: [[0, 1], [0, -1], [1, 0], [-1, 0],
+                      [1, 1], [-1, -1], [1, -1], [-1, 1]],
+
+        frameHandler: function (e) {
+            if (Math.random() < this._moveChance) {
+                this.walk();
+            }
+        },
+
+        walk: function () {
+            if (!this.isPlaying("walk")) {
+                this.playAnimation("walk", 128, -1);
+            }
+
+            if (this._direction[0] > 0) {
+                this.flip("X");
+            } else if (this._direction[0] < 0) {
+                this.unflip("X");
+            }
+
+            if (this._boundary) {
+                if (this._boundary.containsPoint(
+                        this._x + this._direction[0],
+                        this._y + this._direction[1]) &&
+                    this._boundary.containsPoint(
+                        this._x + this._w + this._direction[0],
+                        this._y + this._h + this._direction[1])) {
+                    this.x += this._direction[0];
+                    this.y += this._direction[1];
+                }
+            } else {
+                // naive roaming
+                this.x += this._direction[0];
+                this.y += this._direction[1];
+            }
+
+            if (Math.random() < this._changeDirChance) {
+                this.changeDir();
+            }
+        },
+
+        changeDir: function () {
+            this._direction = this._directions[Math.floor(Math.random() * 8)];
+        },
+
+        setBoundary: function (polygon) {
+            this._boundary = polygon;
         }
     });
 
